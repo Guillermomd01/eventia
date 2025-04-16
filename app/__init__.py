985a484c2cbff055e43462df3b72daa4369de9b0
Guillermo <md.guillermo1@gmail.com>
2025-04-16 09:08:00 +0200

    Rutas del proyecto estrucutradas correctamente con blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
-from flask import Flask,render_template
+from flask import Flask, app ,Blueprint
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-from app.models import Usuario
+
 
 login = LoginManager()
 db = SQLAlchemy()
@@ -9,24 +9,22 @@ db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__,template_folder='templates')
-    @app.route("/")
-    def home():
-        return render_template('index.html')
+    
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+   
     db.init_app(app)
-    
     login.init_app(app)
     login.login_view = 'login'
+    
+    from app.routes import bp as main_bp
+    app.register_blueprint(main_bp)
     
     from app import models
     with app.app_context():
         db.create_all()
-    
-    
+
     return app
 
-@login.user_loader
-def load_user(user_id):
-    return Usuario.query.get(int(user_id))
+
 
--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,44 @@
+from app.models import Usuario
+from  app.forms import FormularioRegistro,FormularioLogin
+from flask import app, flash, redirect,render_template,Blueprint
+from flask_login import login_user
 
+bp = Blueprint('main',__name__)
 
+@bp.route("/")
+def home():
+    return render_template('index.html')
+    
+@bp.route("/registro",methods=['GET','POST'])
+def registro():
+    form_registro = FormularioRegistro()
+    if form_registro.validate_on_submit():
+        nuevo_usuario = Usuario(nombre=form_registro.nombre.data ,email=form_registro.email.data, rol= form_registro.rol.data)
+        nuevo_usuario.set_password(form_registro.password.data)
+        db.session.add(nuevo_usuario)
+        db.session.commit()
+        return redirect('/dashboard')
+    
+    
+    return render_template("registro_form.html",form= form_registro)
+    
+@bp.route("/login",methods=['GET','POST'])
+def login():
+    form_login = FormularioLogin()
+    if form_login.validate_on_submit():
+        usuario = Usuario.query.filter_by(email=form_login.email.data).first()
+        if usuario:
+            if usuario.check_password(form_login.password.data):
+                login_user(usuario) 
+                return redirect('/dashboard')
+            else:
+                flash('Contraseña incorrecta')
+        else:
+            flash('Email incorrecto')
+    
+    return render_template("login_form.html",form= form_login)
+
+from app import login
+@login.user_loader
+def load_user(user_id):
+    return Usuario.query.get(int(user_id))
