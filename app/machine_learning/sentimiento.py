5a3b80c59f17d8508b5dc181dee049c8394ef1ba
Guillermo <md.guillermo1@gmail.com>
2025-04-29 14:00:33 +0200

    funcion para leer y predecir archivos

--- app/machine_learning/sentimiento.py
+++ app/machine_learning/sentimiento.py
@@ -1,1 +1,21 @@
+#creamos funcion que lee los archivos csv y usa el modelo de prediccion
+import joblib
+import pandas as pd
 
+def analisis_sentimientos(csv):
+    
+    modelo = joblib.load('modelo_entrenado_predicion.pkl')
+    tfidf = joblib.load('vectorizador_entrenado.pkl')
+    
+    df = pd.read_csv(csv)
+    
+    X = df['texto']
+    X_vector = tfidf.transform(X)    
+    
+    prediccion = modelo.predict(X_vector)
+    
+    resumen = pd.Series(prediccion).value_counts()
+    return resumen.to_dict()
+    
+    
+    
